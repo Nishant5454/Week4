da9754d98852547a1950e158090797bb409ae95c
Nishant5454 <bhagatnishant5454@gmail.com>
2025-02-16 03:12:49 +0530

    [Nishant]:Add repeating and Extraction code

---
+++ day5/src/main/java/org/example/regex/Advanced/validateIPAddres/ExtractCurrency/extractCurrency.java
@@ -0,0 +1,20 @@
+package org.example.regex.Advanced.validateIPAddres.ExtractCurrency;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+public class extractCurrency {
+
+        public static void main(String[] args) {
+            Pattern p = Pattern.compile("\\$\\d+(\\.\\d{2})?| \\b\\d+\\.\\d{2}\\b");
+            Scanner cs = new Scanner(System.in);
+            System.out.println("Enter a text");
+            String input = cs.nextLine();
+            Matcher m = p.matcher(input);
+            while(m.find()){
+                System.out.println("Matches : " + m.group());
+            }
+        }
+    }
