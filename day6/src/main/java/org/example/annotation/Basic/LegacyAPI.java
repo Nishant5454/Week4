3ac3e8613d89b73ea698126b9844a6657c11dc0c
Nishant5454 <bhagatnishant5454@gmail.com>
2025-02-18 11:57:07 +0530

    [Nishant] Add. Legacy API

--- day6/src/main/java/org/example/annotation/Basic/LegacyAPI.java
+++ day6/src/main/java/org/example/annotation/Basic/LegacyAPI.java
@@ -1,4 +1,26 @@
 package org.example.annotation.Basic;
 
 public class LegacyAPI {
+    @Deprecated
+  //  public static void oldFeature(){
+     //   System.out.println("This is old feature");
+//}
+    //public static void newFeature(){
+      //  System.out.println("This is new Feature");
+  //  }
+
+    public static void main(String[] args) {
+        LegacyAPI.oldfeature();
+        LegacyAPI.newFeature();
+
+    }
+@Deprecated
+    private static void oldfeature() {
+    System.out.println("This is old feature");
+    }
+    private static void newFeature(){
+        System.out.println("This is new feature");
+    }
+
+
 }
